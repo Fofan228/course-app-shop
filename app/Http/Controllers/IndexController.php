217a7d25b524e304e68630c01d45463d79fe620f
Fofan <vovka.322@Mail.ru>
2023-03-30 18:38:19 +0500

    Рефактор и изменение верстки категорий, брендов, товаров. Добавление главной страницы сайта, добавление роута и контроллера для неё

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -1,11 +1,16 @@
 <?php
 
-    namespace App\Http\Controllers;
+namespace App\Http\Controllers;
 
-    class IndexController extends Controller
+use Illuminate\Http\Request;
+
+class IndexController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
     {
-        public function index()
-        {
-            return view('home');
-        }
+        return view('index');
     }
+}
--- routes/web.php
+++ routes/web.php
@@ -7,7 +7,7 @@
     use App\Http\Controllers\RegisterController;
     use Illuminate\Support\Facades\Route;
 
-    Route::get('/', [IndexController::class, 'index'])->name('home');
+    Route::get('/', IndexController::class)->name('index');
 
     Route::middleware("auth")->group(function () {
         Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
